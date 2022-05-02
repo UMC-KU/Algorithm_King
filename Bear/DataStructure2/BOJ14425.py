c8a27e2047a4f0a5bbbabd17e4a26a41e80d0ba2
jungwoo <rlawjddn103@naver.com>
2022-05-02 16:09:06 +0900

    Add DataStructure Boj14425

---
+++ Bear/DataStructure2/BOJ14425.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, sys.stdin.readline().rstrip().split())
+str = set([sys.stdin.readline().rstrip() for _ in range(N)])
+cnt = 0
+
+for _ in range(M):
+
+    temp = sys.stdin.readline().rstrip()
+
+    if temp in str:
+        cnt += 1
+
+print(cnt)
