0f461116a9afb4ce880f42df8acea85ab5caa3ac
jungwookim <rlawjddn103@naver.com>
2022-05-17 14:17:47 +0900

    Add implementation BOJ14719

--- Bear/Implementation/BOJ14719.py
+++ Bear/Implementation/BOJ14719.py
@@ -5,37 +5,13 @@ H,W = map(int,sys.stdin.readline().rstrip().split())
 block = list(map(int,sys.stdin.readline().rstrip().split()))
 
 left = 0
-temp = []
 total = 0
 
 for i in range(len(block)):
-    if i == W-1:
 
-        if left > block[i]:
-            wall = max(temp)
-            
+    max_left = max(block[:i+1])
+    max_right = max(block[i:])
+    m = min(max_left,max_right)
+    total += abs(block[i] - m)
        
-            for rain in range(0,temp.index(wall)):
-                if wall - rain > 0:
-                    print(wall-temp[rain])
-                    total += wall-temp[rain]
-        
-            break
-
-    if left > block[i]:
-        temp.append(block[i])
-    
-    else:
-        wall = min(left,block[i])
-
-        for rain in temp:
-            if wall - rain > 0:
-                print(wall-rain)
-                total += wall-rain
-        
-        temp = []
-        left = block[i]
-       
-
-
 print(total)
